4a1aa363f333330190346f5a14ff40bd87011658
Abbos Uzoqov <abbosuzoqov0777@gmail.com>
2026-09-10 20:35:11 +0500

    git changes

--- app/api/expenses.py
+++ app/api/expenses.py
@@ -25,3 +25,13 @@ def create_expense(expense_data: ExpenseCreate, db: Session = Depends(get_db)):
     db.commit()
     db.refresh(new_expense)
     return new_expense
+@router.put("/expense_id}")
+def put_expense(expense_id: int, expected_data: ExpenseCreate, db: Session = Depends(get_db)):
+    put_expenses = db.query(Expense).filter(Expense.id == expense_id).first()
+    if put_expenses is None:
+        raise HTTPException(status_code=404, detail="Expense not found")
+    put_expenses.amount = expected_data.amount
+    put_expenses.category = expected_data.category
+    db.commit()
+    db.refresh(put_expenses)
+    return put_expenses
